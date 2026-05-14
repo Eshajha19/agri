7985868689c6c6b6aba1b20d342b4b49c726e20d
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-05-14 03:44:00 +0000

    Add additional UI updates and crop grading component

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-8c5d9594'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f9jgejrcd2"
+    "revision": "0.v37g70aspbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
