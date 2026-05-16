53ff2f43cbf51e81b2ec0937de95fcab9a9e6680
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-05-16 13:37:03 +0000

    fixed errors of deployment components

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-8c5d9594'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.lp3i0j1uf3s"
+    "revision": "0.34a1jsrac8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
